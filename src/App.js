fd5b8bfd375256a6c59f57620f7ea320dc8e51ce
Sofiya Tepikin <sofiya.tepikin@gmail.com>
2021-06-13 13:28:46 +0200

    Add filtration functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { useDebounce } from './hooks';
 import { fetchData } from './api';
 
 import Character from './Character';
@@ -6,11 +7,14 @@ import Character from './Character';
 import logo from './logo.svg';
 import './App.css';
 
-async function fetchCharacters(page = 1) {
+async function fetchCharacters(page = 1, nameFilter = '') {
   const query = `
     query {
       characters(
         page: ${page},
+        filter: {
+          name: "${nameFilter}",
+        },
       ) {
         results {
           name,
@@ -27,25 +31,32 @@ async function fetchCharacters(page = 1) {
 function App() {
   const [characters, setCharacters] = useState([]);
   const [page, setPage] = useState(1);
+  const [nameFilter, setNameFilter] = useState('');
+  const debouncedNameFilter = useDebounce(nameFilter, 500);
+
+  const handleNameFilterChange = (e) => setNameFilter(e.target.value);
 
   useEffect(() => {
-    fetchCharacters(page)
+    fetchCharacters(page, debouncedNameFilter)
       .then(res => {
         setCharacters(prevCharacters => [
-          ...prevCharacters,
+          ...(page === 1 ? [] : prevCharacters),
           ...res.characters.results,
         ]);
       });
-  }, [page]);
+  }, [page, debouncedNameFilter]);
+
+  useEffect(() => setPage(1), [debouncedNameFilter]);
 
   return (
     <>
       <header className="header">
         <img className="logo" src={logo} alt="Logo" />
+        <input placeholder="Filter" onChange={handleNameFilterChange} />
       </header>
       <div className="content">
         <h1>Characters Overview ({characters.length})</h1>
-        <ul class="list">
+        <ul className="list">
           {
             characters.map(({ id, name, image, episode = [] }) => (
               <Character
